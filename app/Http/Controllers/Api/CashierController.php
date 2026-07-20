d018fddc407ce987b53527079c619a607e71e6da
junjunghasudungan <junjunghasudungan43@gmail.com>
2026-07-20 10:57:47 +0700

    persiapan pengembangan dashboard cashier

---
+++ app/Http/Controllers/Api/CashierController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CashierController extends Controller
+{
+    public function getCustomer() {
+        try {
+            //code...
+            // mengambil data melalui model
+            $listCustomer = Customer::all();
+
+            return response()->json([
+                'message'=> '',
+                'response' => $listCustomer
+            ], 200);
+
+        } catch (\Throwable $th) {
+           return response()->json([
+            'message'=> $th->getMessage()
+            ], 500);
+        }
+
+    }
+}
