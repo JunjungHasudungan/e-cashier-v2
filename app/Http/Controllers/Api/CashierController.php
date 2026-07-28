0a8398af67c302ab3373c3ad383c28ad866efc76
junjunghasudungan <junjunghasudungan43@gmail.com>
2026-07-28 14:14:00 +0700

    perbaikan tampilan

--- app/Http/Controllers/Api/CashierController.php
+++ app/Http/Controllers/Api/CashierController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\{Validator, DB};
 
 class CashierController extends Controller
 {
@@ -18,7 +19,7 @@ class CashierController extends Controller
             $listCustomer = Customer::all();
 
             return response()->json([
-                'message'=> '',
+                'message'=> 'testing  badai',
                 'response' => $listCustomer
             ], 200);
 
@@ -28,5 +29,54 @@ class CashierController extends Controller
             ], 500);
         }
 
+    }
+
+    public function storeCustomer(Request $request){
+        try{
+             $validator = Validator::make($request->all(), [
+            'company_name' => 'required|unique:customers|min:3',
+            'contact_name' => 'required',
+            'address' => 'required',
+            'city' => 'required',
+            'postal_code' => 'required',
+            'country' => 'required',
+            'phone' => 'required',
+            ],[
+                'company_name.required' => 'nama Perusahaan wajib disi..',
+                'contact_name.required' => 'nama Kontak Wajib dipilh',
+                'address.required' => 'alamat wajib disi..',
+                'city.required' => 'nama kota wajib dipilih..',
+                'postal_code.required' => 'kode pos wajib disi..',
+                'country.required' => 'nama negara wajib disi..',
+                'phone.required' => 'nomor telepon wajib disi..',
+            ]);
+
+             // mengecek jika ada pengiriman yang tidak sesuai required
+            if ($validator->fails()) {
+                return response()->json([
+                    'errors'    => $validator->errors()
+                ], 422);
+            }
+
+            // mengumpulkan seluruh data request kedalam array
+            $validated = $validator->validated();
+
+            // melakukan insert data kedalam table products
+            $customer = DB::table('customers')->insert([
+                'company_name' =>  $validated['company_name'],
+                'contact_name' => $validated['contact_name'],
+                'address' => $validated['address'],
+                'city' => $validated['city'],
+                'postal_code' => $validated['postal_code'],
+                'country' => $validated['country'],
+                'phone' => $validated['phone'],
+            ]);
+
+            // mengembalikan response json
+            return response()->json(['message'   => 'customer berhasil dibuat', 'response'=> $customer], 201);
+
+        }catch(\Exception $error) {
+            return response()->json(['error'   => $error->getMessage()], 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('products', [CashierController::class, 'getListProduct']);
 Route::get('customers', [CashierController::class, 'getCustomer']);
-
+Route::post('customer', [CashierController::class, 'storeCustomer']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
